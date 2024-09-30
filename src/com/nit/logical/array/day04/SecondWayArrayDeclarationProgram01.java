2e5fdeba98dd422ad026e6be23538f42bf4605ff
Gokul <gokullade89@gmail.com>
2024-10-01 02:14:00 +0530

    Second Way of Array Creation

---
+++ src/com/nit/logical/array/day04/SecondWayArrayDeclarationProgram01.java
@@ -0,0 +1,12 @@
+package com.nit.logical.array.day04;
+/*
+ * 
+ * */
+public class SecondWayArrayDeclarationProgram01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
