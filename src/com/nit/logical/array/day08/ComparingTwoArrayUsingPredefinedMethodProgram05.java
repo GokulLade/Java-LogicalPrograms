e3fbcb3964c3c106d747cd7834219f6cf767e692
Gokul <gokullade89@gmail.com>
2024-10-05 03:55:44 +0530

    comparing two array using Arrays class Pre-defined method

---
+++ src/com/nit/logical/array/day08/ComparingTwoArrayUsingPredefinedMethodProgram05.java
@@ -0,0 +1,17 @@
+package com.nit.logical.array.day08;
+
+import java.util.Arrays;
+
+public class ComparingTwoArrayUsingPredefinedMethodProgram05 {
+
+	public static void main(String[] args) 
+	{
+	
+		int arr1[]= {1,2,3,45,67,8};
+		int arr2[]= {1,2,45,67,8};
+		
+		System.out.println(Arrays.compare(arr1, arr2));
+
+	}
+
+}
